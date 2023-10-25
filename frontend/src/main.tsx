feae4e2e38c774e0dd2608aec380310ee0e0f112
itmaybehimm <himansupradhan472@gmail.com>
2023-10-25 20:39:17 +0545

    ts config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App.tsx";
+import "./index.css";
+import ErrorPage from "./pages/ErrorPage.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router: any = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-3xl font-bold underline">Hello world!</div>,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ frontend/src/pages/ErrorPage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useRouteError } from "react-router-dom";
+
+interface ErrorPageInterface {
+  statusText: string;
+  message: string;
+}
+
+const ErrorPage: React.FC = function () {
+  const error: ErrorPageInterface = useRouteError() as ErrorPageInterface;
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+};
+
+export default ErrorPage;
